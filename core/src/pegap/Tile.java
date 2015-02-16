a31e90f6ac990dcd133f6a5490a7146ddbdd05ff
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-02-16 13:44:29 -0800

    Added position code to Tile class

--- core/src/pegap/Tile.java
+++ core/src/pegap/Tile.java
@@ -3,17 +3,29 @@ package pegap;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Tile extends Actor {
+	private Vector2 pos;
 	private Texture texture;
 
 	Tile() {
+		setPos(0.0f, 0.0f);
 		setTexture("img/tiles/tile.png");
 	}
 
-	Tile(String fname) {
+	Tile(float y, float x, String fname) {
+		setPos(x, y);
 		setTexture(fname);
+	}
+
+	public void setPos(float x, float y) {
+		pos = new Vector2(x, y);
+	}
+
+	public void setPos(Vector2 v) {
+		pos = new Vector2(v);
 	}
 
 	public void setTexture(String fname) {
@@ -22,6 +34,6 @@ public class Tile extends Actor {
 
 	@Override
 	public void draw(Batch batch, float alpha) {
-		batch.draw(texture, 0, 0);
+		batch.draw(texture, pos.x, pos.y);
 	}
 }
