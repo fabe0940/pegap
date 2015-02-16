b5535963757613fe46c93d5661d317f03aaebda8
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-02-16 14:24:51 -0800

    Created actor group

--- core/src/pegap/PegaPuzzle.java
+++ core/src/pegap/PegaPuzzle.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -13,18 +14,22 @@ import pegap.Tile;
 
 public class PegaPuzzle implements ApplicationListener {
 	private Stage S;
+	private Group G;
 
 	@Override
 	public void create() {
 		S = new Stage(new FitViewport(800, 600));
+		G = new Group();
 
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
 				Tile t = new Tile();
 				t.setPos((float) i, (float) j);
-				S.addActor(t);
+				G.addActor(t);
 			}
 		}
+
+		S.addActor(G);
 	}
 
 	@Override
